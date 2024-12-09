aeb8163bde4ea689b62a8ec13002c3fa80f8fede
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-12-09 12:49:42 +0500

    MongoDB connected

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const dotenv = require("dotenv").config();
 const app = express();
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
+const Mongo_DB_URL = process.env.MONGO_DB_URL;
 
+//  MongoDB
+mongoose.connect(Mongo_DB_URL)
+.then(()=>{
+    console.log("MongoDB connected successfully")
+}).catch((error)=>{
+    console.log(`Failed to connect MongoDB ${error.message}`)
+});
+
+// server
 app.listen(PORT, ()=>{
     console.log(`Server Running on : ${PORT}`)
 })
